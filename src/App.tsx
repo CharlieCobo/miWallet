8742e052a937013c32842f2da33131f561cdaf91
charlie.cobo <charlie.cobo@gmail.com>
2025-05-21 07:03:29 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { HeroUIProvider } from '@heroui/react';
+import Acordion from './commons/ui/components/acordion';
+
+function App() {
+  return (
+    <HeroUIProvider>
+      <Acordion />
+    </HeroUIProvider>
+  );
+}
+
+export default App;
